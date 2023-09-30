878e4f903ef95d22e85bc099c7d178e1798976bb
morawararuna <146064070+morawararuna@users.noreply.github.com>
2023-09-30 08:46:55 +0530

    Add files via upload

---
+++ ControlStatements.java
@@ -0,0 +1,31 @@
+package arunaapplication;
+
+public class ControlStatements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*int a=100,b=123,c=376,max,min;
+if(c>a){
+	max=c;
+min=a;}
+else {max=a;
+min=c;}
+if(b>max) {max=b;}
+if(b<min) {
+	min=b;}
+System.out.println("min----->"+min);
+System.out.println("max------>"+max);
+}
+	}*/
+		//loops
+		//do...while()...
+		//while
+		//for
+		int i=1,j=100;
+		while(j>=10) {
+			System.out.println(i);
+		j=j-10;//2 3 4 5 6
+		i++;
+	}}
+}
+
