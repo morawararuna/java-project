043a7ea28be4878862201bb496f6d0a4bc07325f
morawararuna <146064070+morawararuna@users.noreply.github.com>
2023-09-30 08:36:05 +0530

    Add files via upload

---
+++ maxmum.java
@@ -0,0 +1,19 @@
+package arunaapplication;
+
+public class maxmum{
+	public static void main(String arg[] )
+	{	 
+	int a=234,b=434,c=678,max=0;
+	if((a>b)&&(a>c)) {
+		max=a;
+	}
+		if((b>a)&&(b>c)) {
+			max=b;
+		}
+	 if((c>a)&&(c>b)) {
+		max =c;
+	} System.out.println("maxvalue----"+max);
+
+	}
+	}
+
