f05d833bff61d390d7839753ca216ba76b6dba85
morawararuna <146064070+morawararuna@users.noreply.github.com>
2023-09-30 08:35:30 +0530

    Add files via upload

---
+++ oddnumbers.java
@@ -0,0 +1,22 @@
+package arunaapplication;
+
+public class oddnumbers {
+
+	public static void main(String[] args) { 
+	/*int i=1;
+	while(i<=10)
+	{
+System.out.println(i);
+i=i+2;
+	}*/
+		/*int i=1;
+		while(i<=10) {
+		if(i%2!=0) {*/
+		int i=1;
+		while(i<=10) {
+			if(i%2!=0) {
+			System.out.println(i);
+	
+	}i++;}
+		
+	}}
