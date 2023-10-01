947ea7f7a1ba2d0330fa8e718f9d1c048bbf9b89
morawararuna <146064070+morawararuna@users.noreply.github.com>
2023-10-01 14:58:23 +0530

    Update ControlStatements.java

--- ControlStatements.java
+++ ControlStatements.java
@@ -4,7 +4,7 @@ public class ControlStatements {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-/*int a=100,b=123,c=376,max,min;
+int a=100,b=123,c=376,max,min;
 if(c>a){
 	max=c;
 min=a;}
@@ -16,16 +16,4 @@ if(b<min) {
 System.out.println("min----->"+min);
 System.out.println("max------>"+max);
 }
-	}*/
-		//loops
-		//do...while()...
-		//while
-		//for
-		int i=1,j=100;
-		while(j>=10) {
-			System.out.println(i);
-		j=j-10;//2 3 4 5 6
-		i++;
-	}}
-}
-
+	}
