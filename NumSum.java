c90b44824e7bfe358148102addbcf5ac41fa15e9
morawararuna <146064070+morawararuna@users.noreply.github.com>
2023-10-01 15:38:01 +0530

    Update NumSum.java

--- NumSum.java
+++ NumSum.java
@@ -2,12 +2,14 @@ package arunaapplication;
 
 public class NumSum {
 	public static void main(String[] args) {
+		int Sum=1+2+3+4+5+6+7+8+9;
 int num=8431,r,noSum=0,res=0;
 while(num!=0) {
 	r=num%10;
 num=num/10;
 noSum=noSum+r; 
 }
-System.out.println("nosum-----"+ noSum);
+res=Sum-noSum;
+System.out.println("res-----"+ res);
 }	
 }
