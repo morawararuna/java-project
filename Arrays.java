f914f829890bf530a1337349062b2bb884a7568e
morawararuna <146064070+morawararuna@users.noreply.github.com>
2023-10-03 09:22:29 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,22 @@
+package arunaapplication;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		int n[]=new int[10];
+		n[0]=
+				n[1]=5;
+				n[2]=10;
+				n[3]=15;
+				n[4]=20;
+				n[5]=25;
+				n[6]=45;
+				n[7]=78;
+				n[8]=87;
+				n[9]=96;
+				for( int i=0;i<10;i++) {
+				System.out.println(n[i]+"");
+
+	}}
+
+}
