e10f0750dd61330ad19751ce9848809d5cb2046d
morawararuna <146064070+morawararuna@users.noreply.github.com>
2023-10-03 09:58:22 +0530

    Update ArrayAddtion.java

--- ArrayAddtion.java
+++ ArrayAddtion.java
@@ -5,7 +5,7 @@ public class ArrayAddtion {
 	public static void main(String[] args) {
 		int[] n= {110,245,678,765,45};
 		int[] m= {67,76,46,12,8,65,98,1,789};
-		int[] p;// n+m
+		int[] p;    // n+m
 		p = new int[n.length+m.length];
 		int pIndex=0;
 		for(int i=0;i<n.length;i++,pIndex++) {
