44ae10783200431e07e0a5a6ef06d8c0ceafa318
morawararuna <146064070+morawararuna@users.noreply.github.com>
2023-10-03 09:45:04 +0530

    Update ArraysNumbers.java

--- ArraysNumbers.java
+++ ArraysNumbers.java
@@ -6,7 +6,7 @@ public class ArraysNumbers {
 		int[] n= {7,9,13,25,67,4,2,1,0,456,876,87654};
 		int min,max;
 		
-		min=max=n[0];//insted of 0 we can take any number index 
+		min=max=n[n.length-1];//insted of 0 we can take any number index 
 		//for(int i=1;i<n.length;i++)
 		for(int i=n.length-1;i>=0;i--) {
 			if(n[i] > max) max= n[i];
