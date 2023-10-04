55334d0ba6cedde6d13431774173882d8cca8abe
morawararuna <146064070+morawararuna@users.noreply.github.com>
2023-10-04 10:16:27 +0530

    Add files via upload

---
+++ LeftTrianglePattern1.java
@@ -0,0 +1,18 @@
+package arunaapplication;
+
+public class LeftTrianglePattern1 {
+
+	public static void main(String[] args) {
+		// * 1
+				 //* 1 2
+				 //* 1 2 3
+				 //* 1 2 3 4
+				 //* 1 2 3 4 5 
+				 for(int r=1;r<=5;r++) {
+					for(int c=1;c<=r;c++) {
+						System.out.print(c+" ");
+					}
+					System.out.println();
+				}}}
+
+
