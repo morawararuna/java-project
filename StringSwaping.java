b2e9da949c459526d72bc420c291c03a3afe62fc
morawararuna <146064070+morawararuna@users.noreply.github.com>
2023-10-06 10:33:25 +0530

    Add files via upload

---
+++ StringSwaping.java
@@ -0,0 +1,20 @@
+package arunaapplication;
+
+public class StringSwaping {
+
+	public static void main(String[] args) {
+		String s1 = "hyderabad";
+		String s2 = "secendrabad";
+		System.out.println("Before Swapping....");
+		System.out.println("s1--->"+s1);
+		System.out.println("s2--->"+s2);
+		s1 = s1 + s2;
+		s2 = s1.substring(0, s1.length()-s2.length());
+		s1= s1.substring(s2.length());
+		System.out.println("After Swapping....");
+		System.out.println("s1--->"+s1);
+		System.out.println("s2--->"+s2);
+
+	}
+
+}
