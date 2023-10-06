480b2be099171774e1f745b459c2da2c69f544c9
morawararuna <146064070+morawararuna@users.noreply.github.com>
2023-10-06 11:00:43 +0530

    Add files via upload

---
+++ StringUpperLowerCase.java
@@ -0,0 +1,22 @@
+package arunaapplication;
+
+public class StringUpperLowerCase {
+
+	public static void main(String[] args) {
+		//G--65,66
+		//g----97,98
+		
+			char ch = 'G';
+			ch = (char)(ch+32);
+		System.out.println(ch);
+		ch=(char)(ch-32);
+		System.out.println(ch);
+		char ch2='H';
+		ch2=(char)(ch2+32);
+		System.out.println(ch2);
+		ch2=(char)(ch2-32);
+		System.out.println(ch2);
+
+	}
+
+}
