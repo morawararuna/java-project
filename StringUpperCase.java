84d625958a13f5d49f910949d95564cd084d586c
morawararuna <146064070+morawararuna@users.noreply.github.com>
2023-10-06 11:09:29 +0530

    Add files via upload

---
+++ StringUpperCase.java
@@ -0,0 +1,21 @@
+package arunaapplication;
+
+public class StringUpperCase {
+
+	public static void main(String[] args) {
+		String str = "Welcome to hyd";
+		// WELCOME TO HYD
+		// A--Z--65--90
+		// a--97---122
+		
+		char[] data = str.toCharArray();
+		String res = "";
+		for(int i=0;i<data.length;i++) {
+			if( (data[i] >=97) &&(data[i] <=122)) {
+				data[i] = (char)(data[i]- 32);
+
+	}
+res=res+data[i];
+}
+		System.out.println(res);
+		}}
