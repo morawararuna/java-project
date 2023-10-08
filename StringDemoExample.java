87a03166311b2c5cf2e88ffba482735429dec651
morawararuna <146064070+morawararuna@users.noreply.github.com>
2023-10-08 19:34:57 +0530

    Add files via upload

---
+++ StringDemoExample.java
@@ -0,0 +1,12 @@
+package strings1;
+
+public class StringDemoExample {
+
+	public static void main(String[] args) {
+		String str="gauraansh";
+System.out.println(str);
+str=str.concat("devulapally");
+System.out.println(str);
+	}
+
+}
